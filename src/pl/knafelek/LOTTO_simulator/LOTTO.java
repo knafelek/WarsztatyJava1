eed259c45d28b82012ae64d32f0ebd51ad4d9c55
Gabriela Knafel <gabriela.knafel@gmail.com>
2020-05-16 17:57:36 +0200

    Primary functionality

--- src/pl/knafelek/LOTTO_simulator/LOTTO.java
+++ src/pl/knafelek/LOTTO_simulator/LOTTO.java
@@ -2,16 +2,36 @@ package pl.knafelek.LOTTO_simulator;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class LOTTO {
 
-    public static void main(String [] args){
+    public static void main(String [] args) throws InterruptedException {
 
         int [] numbers = new int [49];
         int nextVal = 1;
         for(int i = 0; i< numbers.length; i++){
             numbers[i] = nextVal++;
         }
+
+        System.out.println("Za chwilę wylosujemy 6 spośród liczb: ");
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("Zgadnij, jakie to będą liczby: ");
+
+        Scanner scan = new Scanner(System.in);
+
+        int [] guessNumbers = new int [6];
+        for (int i=0; i < guessNumbers.length; i++){
+            guessNumbers[i] = scan.nextInt();
+        }
+
+        Arrays.sort(guessNumbers);
+        System.out.println("Wytypowałeś następujące liczby: " + Arrays.toString(guessNumbers));
+
+        System.out.println("Następuje zwolnienie blokady!");
+
+        Thread.sleep(4000);
 
         int scope = numbers.length; // znacznik - na jakiej kulce skończyliśmy
         int[] drawnNumbers = new int [6];
@@ -27,12 +47,29 @@ public class LOTTO {
             scope--; //algorytm niewydajny pamięciowo ale wydajny jeśli mamy mało liczb
         }
 
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
-
+        System.out.println("Wylosowano następujące liczby: ");
         Arrays.sort(drawnNumbers);
         System.out.println(Arrays.toString(drawnNumbers));
+
+        int guessCounter = 0;
+        for (int i=0; i<guessNumbers.length; i++){
+            for(int j=0; j<drawnNumbers.length; j++){
+                if (guessNumbers[i] == drawnNumbers[j]){
+                    guessCounter+=1;
+                } else guessCounter +=0;
+            }
+        }
+        Thread.sleep(2000);
+        if (guessCounter<3){
+            System.out.println("Nie tym razem! Nie trafiłeś nawet trójki! Spróbuj szczęścia jeszcze raz!");
+        } else if (guessCounter<5){
+            System.out.println("Udało Ci się trafić " + guessCounter + " liczby.");
+        } else {
+            System.out.println("Gratulacje! Udało Ci się trafić " + guessCounter + " liczb.");
+        }
 
     }
 
 }
+
+
